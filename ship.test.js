7fa19194b7a6fdc2e44c8dc55c7d7216a0c15b38
morris <morristwinomugisha2000@gmail.com>
2024-11-05 08:23:45 +0300

    feat: implement the ship class implement both the hit and length methods of the ship class

---
+++ ship.test.js
@@ -0,0 +1,23 @@
+const Ship = require('./ship');
+
+test('creates a ship with the correct length and zero hits', () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+    expect(ship.hits).toBe(0);
+});
+
+test('hit() increments the number of hits', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+    ship.hit();
+    expect(ship.hits).toBe(2);
+});
+
+test('isSunk() returns true if the ship is sunk', () => {
+    const ship = new Ship(2);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+});
